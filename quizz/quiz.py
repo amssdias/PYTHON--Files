4650143e53db938bc03de76a72ab93b53a3d236a
André Dias <andremssdias@hotmail.com>
2021-01-16 22:31:35 +0100

    Small quizz to check answers of user

--- quizz/quiz.py
+++ quizz/quiz.py
@@ -2,11 +2,23 @@ question = open('questions.txt', 'r')
 question_content = [question.strip().split("=") for question in question.readlines()]
 question.close()
 
-print(question_content)
-
 question_content = {
     question[0]:question[1]
     for question in question_content
 }
 
-print(question_content)
+answers = []
+
+for key in question_content:
+    answer = input(key + "=")
+    answers.append(answer)
+
+n = 0
+
+for index, key in enumerate(question_content):
+    if question_content[key] == answers[index]:
+        n += 1
+
+result = open('result.txt', 'w')
+result.write(f"Your final score is {n}/{len(question_content)}.")
+result.close()
